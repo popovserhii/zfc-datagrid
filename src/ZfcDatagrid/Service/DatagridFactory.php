339d2161588243ed97a61fb9036ee552fed10ae0
Serhii Popov <popow.serhii@gmail.com>
2025-02-27 11:57:47 +0200

    Upgrade PHP to 7.4

--- src/ZfcDatagrid/Service/DatagridFactory.php
+++ src/ZfcDatagrid/Service/DatagridFactory.php
@@ -4,6 +4,7 @@ namespace ZfcDatagrid\Service;
 use InvalidArgumentException;
 use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
+use Laminas\Cache\Storage\AdapterPluginManager;
 use ZfcDatagrid\Datagrid;
 use ZfcDatagrid\Middleware\RequestHelper;
 use ZfcDatagrid\Router\RouterInterface;
@@ -38,6 +39,22 @@ class DatagridFactory
         $grid->setOptions($config['ZfcDatagrid']);
         $grid->setRequest($request);
         $grid->setRouter($router);
+
+        #$this->setCache(Cache\StorageFactory::factory($options['cache']));
+        $cacheConfig = $config['ZfcDatagrid']['cache'];
+//        $cacheConfig['adapter'] = $cacheConfig['adapter']['name'];
+//        $adapterPluginManager = new AdapterPluginManager($container);
+//        $cache = $adapterPluginManager->get(
+//            $cacheConfig['adapter'],
+//            $cacheConfig
+//        );
+
+        //$cache = new \Laminas\Cache\Storage\Adapter\Filesystem([], null);
+        $cache = new \Laminas\Cache\Storage\Adapter\Filesystem();
+        $cache->addPlugin(new \Laminas\Cache\Storage\Plugin\Serializer());
+
+        $grid->setCache($cache);
+
 
         if (true === $container->has(TranslatorInterface::class)) {
             $grid->setTranslator($container->get(TranslatorInterface::class));
