ee146da6570dbf234e3bf77bb1aea98c23104f88
Serhii Popov <popow.serhii@gmail.com>
2025-06-18 13:53:52 +0300

    Add possibility to pass filters through the Headers

--- src/ZfcDatagrid/Renderer/JqGrid/Renderer.php
+++ src/ZfcDatagrid/Renderer/JqGrid/Renderer.php
@@ -64,6 +64,27 @@ class Renderer extends AbstractRenderer
     }
 
     /**
+     * Filters can be applied only to a "simple" column.
+     * If a column has an Expression under the hood then this column should be ignored in the following fiters appling.
+     * 
+     * @param $column
+     *
+     * @return bool
+     */
+    public function isColumnFilterable($column)
+    {
+        if (!$column) {
+            return false;
+        }
+
+        if (!is_string($column->getSelectPart1()) || !is_string($column->getSelectPart2())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
      * Get or create a column by name.
      *
      * $name has to be in the format: "aliasName.columnName".
@@ -73,8 +94,11 @@ class Renderer extends AbstractRenderer
     public function getColumnByName($name)
     {
         $uniqueId = $this->actualizeUniqueId($name);
-        [$tableAlias, $columnName] = explode('.', $name);
-        $column = $this->getColumn($uniqueId) ?: $this->createColumn($columnName, $tableAlias);
+        $column = $this->getColumn($uniqueId);
+        if (!$this->isColumnFilterable($column)) {
+            [$tableAlias, $columnName] = explode('.', $name);
+            $column = $this->createColumn($columnName, $tableAlias);
+        }
 
         return $column;
     }
@@ -89,11 +113,13 @@ class Renderer extends AbstractRenderer
         $postParams = $request->getParsedBody();
         $queryParams = $request->getQueryParams();
 
-        $sortColumns = $postParams[$parameterNames[$paramName]]
+        // Checking order: POST -> GET -> Headers
+        $param = $postParams[$parameterNames[$paramName]]
             ?? $queryParams[$parameterNames[$paramName]]
+            ?? $request->getHeaderLine($parameterNames[$paramName])
             ?? null;
 
-        return $sortColumns;
+        return $param;
     }
 
     public function getGroupColumns()
